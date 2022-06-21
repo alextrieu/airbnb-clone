1007b73b92d8214db30bcee5e22a72fb90c7f55a
Alex <alextrieu52@gmail.com>
2022-06-20 18:56:43 -0700

    added a route to redirect to a new page: room

--- src/components/homepage/Card.js
+++ src/components/homepage/Card.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SimpleImageSlider from "react-simple-image-slider";
+import { useNavigate } from "react-router-dom";
 
 export default function Card(props) {
 
@@ -13,32 +14,40 @@ export default function Card(props) {
       img
   } = props.listing
 
+  // const history = useHistory()
+
+  // const navigate =() => history.push('/room')
+
+  const navigate = useNavigate();
+
+
+
   return (
     <div className='card-container'>
-      <div className='card-image'>
-        <SimpleImageSlider
-          width={300}
-          height={290}
-          images={img}
-          showBullets={true}
-          showNavs={true}
-          navMargin={1}
-          navSize={30}
-        />
-      </div>
-      <div className='card-description'>
-        <div className='card-title'>
-          <span className='card-location'>{city}, {state}</span>
-          <span className='card-rating'>{rating}<i className="fa-solid fa-star fa-sm"></i></span>
+        <div className='card-image'>
+          <SimpleImageSlider
+            width={300}
+            height={290}
+            images={img}
+            showBullets={true}
+            showNavs={true}
+            navMargin={1}
+            navSize={30}
+          />
         </div>
-        <div className='card-distance-date'>
-          <p>{distance}</p>
-          <p>{date}</p>
+        <div className='card-description' onClick={() => navigate('/room')}>
+          <div className='card-title'>
+            <span className='card-location'>{city}, {state}</span>
+            <span className='card-rating'>{rating}<i className="fa-solid fa-star fa-sm"></i></span>
+          </div>
+          <div className='card-distance-date'>
+            <p>{distance}</p>
+            <p>{date}</p>
+          </div>
+          <div className='card-price'>
+            <p><span>${price}</span> night</p>
+          </div>
         </div>
-        <div className='card-price'>
-          <p><span>${price}</span> night</p>
-        </div>
-      </div>
     </div>
   )
 }
