33186b04e23da0c27a91b405043a10b06764d5d6
Alex <alextrieu52@gmail.com>
2022-06-21 18:24:44 -0700

    added navigation template

--- src/components/homepage/Feed.js
+++ src/components/homepage/Feed.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card.js';
 import { listings } from '../data/listings.js';
-
+import Navigation from './Navigation.js';
 
 function Feed() {
   
@@ -9,9 +9,12 @@ function Feed() {
 
 
   return (
-    <div className='listing-container'>
-      {cardElements}
-    </div>
+    <>
+      <Navigation />
+      <div className='listing-container'>
+        {cardElements}
+      </div>
+    </>
     
   );
 }
---
+++ src/components/homepage/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav>
+        <div className='logo'>Logo</div>
+        <div className='filter'>
+            <div>Anywhere</div>
+            <div>Any week</div>
+            <div>Add guests</div>
+        </div>
+        <div className='profile-setting'>
+            <div>Become a Host</div>
+            <div>Globe</div>
+            <div className='navbar'>Navbar</div>
+        </div>
+    </nav>
+  )
+}
