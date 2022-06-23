f6251f350a6357efbd321e2f38d9a27ba184dade
Alex <alextrieu52@gmail.com>
2022-06-22 17:15:56 -0700

    updated navigation bar

--- src/components/homepage/Navigation.js
+++ src/components/homepage/Navigation.js
@@ -3,16 +3,26 @@ import React from 'react'
 export default function Navigation() {
   return (
     <nav>
-        <div className='logo'>Logo</div>
+        <div className='logo'>
+          <img src='/logo.png'></img>
+        </div>
         <div className='filter'>
             <div>Anywhere</div>
             <div>Any week</div>
             <div>Add guests</div>
+            <div><i class="fa-solid fa-magnifying-glass"></i></div>
         </div>
         <div className='profile-setting'>
-            <div>Become a Host</div>
-            <div>Globe</div>
-            <div className='navbar'>Navbar</div>
+            <div className='host-setting'>
+              Become a Host
+            </div>
+            <div className='language-setting'>
+              <i class="fa-solid fa-globe"></i>
+            </div>
+            <div className='navbar'>
+              <i class="fa-solid fa-bars"></i>
+              <i class="fa-regular fa-user"></i>
+            </div>
         </div>
     </nav>
   )
